3643293633c6d0591b62b474ae3e34d4bc747140
sekterra <sekterra@nate.com>
2018-07-10 09:14:47 +0900

    SideDrawer style 적용(테스트)

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -3,7 +3,7 @@
     <ActionBar class="action-bar" title="Home"/>
 
     <StackLayout>
-      <Button class="btn btn-primary" @tap="$router.push('/counter')">Counter</Button>
+      <Button class="btn btn-primary" @tap="$router.push('/counter')">Counter2</Button>
       <Button class="btn btn-primary" @tap="$router.push('/hello')">Hello World</Button>
       <Button class="btn btn-primary" @tap="$router.push('/samples/sideDrawer')">sideDrawer</Button>
     </StackLayout>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
 import Vue from 'nativescript-vue';
 import router from './router';
+// import './app.css';
 import './styles.scss';
+
 
 // sideDrawer 컴포넌트  전역에서 사용할 수 있도록 등록
 Vue.registerElement('RadSideDrawer', () => require('nativescript-ui-sidedrawer').RadSideDrawer)
